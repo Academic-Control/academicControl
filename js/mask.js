782b2ba7d4849fd0eac8d5e28cac9635f3c86060
LaisFranco <lah_spfc2010@hotmail.com>
2015-06-17 11:30:39 -0300

    validacao de dados
    validação do cpd, email

--- js/mask.js
+++
@@ -1,9 +0,0 @@
-
- (document).ready(function(){
-   ("input.data").mask("99/99/9999");
-   ("input.cpf").mask("999.999.999-99");
-   ("input.cep").mask("99.999-999");
-   ("input.rg").mask("99.999.999");
-   ("input.tel").mask("(99)9999-9999");
-                      
-});
