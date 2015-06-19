83cc9bb4b557d612ea1ba4e60f7642ab0747169f
Andressa <andressa_faria18@hotmail.com>
2015-06-18 23:37:53 -0300

    Login
    Formulário e ação login

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Academico</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body>		
+	     Área restrita
+		<div>
+				<h1>Academic Control</h1>
+		</div>
+			<div align="center">
+			  <table width="37%" border="0" cellpadding="0" cellspacing="0" bordercolor="#000000">
+			    <form method="post" action="login_vai.php">
+			      <tr>
+			        <td height="39" colspan="3"> &Aacute;rea restrita</td>
+      </tr>
+			      <tr>
+      <td width="18%" height="46">Login:</td>
+      
+	<td width="53%"><input type="text" name="login" /></td>
+    </tr>
+			      <tr>
+			        <td height="41">Senha:</td>
+      <td><input type="password" name="senha" /></td>
+    </tr>
+			      <tr>
+			        <td height="28" colspan="2"><div align="center">
+			          <input type="submit" value="Enviar" />
+		            </div></td>
+      </tr>
+		        </form>
+              </table>
+			  
+			 
+</div>
+</div>
+	</div>
+</body>
+</html>
